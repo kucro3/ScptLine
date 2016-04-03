15bd428864cf88b93461d3898fcd40bad41ca33a
Kumonda221 <kumonda@foxmail.com>
2016-04-03 16:40:58 +0800

    First push.

--- ScptLine/src/org/kucro3/scptline/SLDefinitionMap.java
+++ ScptLine/src/org/kucro3/scptline/SLDefinitionMap.java
@@ -19,6 +19,11 @@ public class SLDefinitionMap implements SLObject {
 		return env;
 	}
 	
+	public boolean defined(String name)
+	{
+		return map.containsKey(name);
+	}
+	
 	final boolean requireCheck()
 	{
 		return env.getState() != SLEnvState.BOOTING;
@@ -36,6 +41,47 @@ public class SLDefinitionMap implements SLObject {
 	final boolean duplicated(String name)
 	{
 		return map.containsKey(name);
+	}
+	
+	public static class SLDefinitionException extends SLException
+	{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 6506514705626273741L;
+		
+		public SLDefinitionException(SLEnvironment env, SLExceptionLevel level,
+				String stub)
+		{
+			super(env, level, DESCRIPTION, stub);
+		}
+		
+		public SLDefinitionException(SLEnvironment env, SLExceptionLevel level,
+				String stub, String message)
+		{
+			super(env, level, DESCRIPTION, stub, message);
+		}
+		
+		public static SLDefinitionException newVarDuplicated(SLEnvironment env, String name)
+		{
+			return new SLDefinitionException(env, SLExceptionLevel.INTERRUPT,
+					MESSAGE_DEFINITION_DUPLICATED,
+					String.format(MESSAGE_DEFINITION_DUPLICATED, name));
+		}
+		
+		public static SLDefinitionException newVarUndefined(SLEnvironment env, String name)
+		{
+			return new SLDefinitionException(env, SLExceptionLevel.INTERRUPT,
+					MESSAGE_UNDEFINED,
+					String.format(MESSAGE_UNDEFINED, name));
+		}
+		
+		public static final String MESSAGE_UNDEFINED = "Undefined variable: %s";
+		
+		public static final String MESSAGE_DEFINITION_DUPLICATED = "Variable redefinition: %s";
+		
+		public static final String DESCRIPTION = "An exception occurred in definition pool.";
 	}
 	
 	private final Map<String, RefObject> map;
